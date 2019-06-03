74b261ef12236d7f6f626b7be7a79a69d5c0854b
taaviKase <taavikase@gmail.com>
2019-06-03 14:11:36 +0300

    Pandas

--- src/root/nested/example.py
+++ src/root/nested/example.py
@@ -307,6 +307,34 @@ np_weight_lbs = np_weight_kg * 2.2
 print(np_weight_lbs)
 
 print
-print("12")
+print("12 Pandas Basics")
+print
+
+'''
+Here we have a problem with importing pandas
+
+dict = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
+       "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
+       "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+       "population": [200.4, 143.5, 1252, 1357, 52.98] }
+
+import pandas as pd
+brics = pd.DataFrame(dict)
+print(brics)
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out observation for Japan
+print(cars.iloc[2])
+
+# Print out observations for Australia and Egypt
+print(cars.loc[['AUS', 'EG']])
+'''
+print("pandas end here")
+
+print
+print("13 Generators")
 print
 
