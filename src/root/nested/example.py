6180fddfa353e1f2ad1436c26e42b8ffb7976595
taaviKase <taavikase@gmail.com>
2019-06-03 15:19:46 +0300

    Regex

--- src/root/nested/example.py
+++ src/root/nested/example.py
@@ -393,6 +393,25 @@ if bar(1,2,3,magicnumber = 7) == True:
     print("Awesome!")
 
 print
-print("15 Multiple Function Arguments")
+print("16 Regex")
 print
 
+# Exercise: make a regular expression that will match an email
+import re
+def test_email(your_pattern):
+    pattern = re.compile(your_pattern)
+    emails = ["john@example.com", "python-list@python.org", "wha.t.`1an?ug{}ly@email.com"]
+    for email in emails:
+        if not re.match(pattern, email):
+            print("You failed to match %s" % (email))
+        elif not your_pattern:
+            print("Forgot to enter a pattern!")
+        else:
+            print("Pass")
+# Your pattern here!
+pattern = r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?"
+test_email(pattern)
+
+print
+print("17 Exception Handling")
+print
