2326ea2f94d927dccbff08293aa728fc2fcaf1cc
taaviKase <taavikase@gmail.com>
2019-06-03 11:02:06 +0300

    classes

--- src/root/nested/example.py
+++ src/root/nested/example.py
@@ -203,6 +203,36 @@ def name_the_benefits_of_functions():
 name_the_benefits_of_functions()
 
 print
-print("8")
+print("8 classes and functions come hereafter")
 print
 
+# define the Vehicle class
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+# your code goes here
+
+car1 = Vehicle()
+car1.kind = "convertible"
+car1.name = "Fer"
+car1.color = "red"
+car1.value = 60000.00
+
+car2 = Vehicle()
+car2.kind = "van"
+car2.name = "Jump"
+car2.color = "blue"
+car2.value = 10000.00
+
+# test code
+print(car1.description())
+print(car2.description())
+
+print
+print("9")
+print
