bff513200747ab604ac15d4ac3773c230241a856
taaviKase <taavikase@gmail.com>
2019-06-04 12:38:04 +0300

    JSON

--- src/root/nested/example.py
+++ src/root/nested/example.py
@@ -462,3 +462,27 @@ print
 print("19 Serialization")
 print
 
+import json
+#import pickle
+
+# fix this function, so it adds the given name
+# and salary pair to salaries_json, and return it
+def add_employee(salaries_json, name, salary):
+    salaries = json.loads(salaries_json)
+    salaries[name] = salary
+
+    return json.dumps(salaries)
+
+# test code
+salaries = '{"Alfred" : 300, "Jane" : 400 }'
+new_salaries = add_employee(salaries, "Me", 800)
+decoded_salaries = json.loads(new_salaries)
+print(decoded_salaries["Alfred"])
+print(decoded_salaries["Jane"])
+print(decoded_salaries["Me"])
+#print(pickle.loads(new_salaries))
+print(new_salaries)
+
+print
+print("20")
+print
