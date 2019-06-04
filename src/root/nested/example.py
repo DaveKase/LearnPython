d803c311d8afe5875e931217121bf46c581863c5
taaviKase <taavikase@gmail.com>
2019-06-04 14:17:16 +0300

    End of tutorials commit

--- src/root/nested/example.py
+++ src/root/nested/example.py
@@ -526,3 +526,31 @@ print(multiplywith5(9))
 print
 print("23 Decorators")
 print
+
+def type_check(correct_type):
+    def check(old_function):
+        def new_function(arg):
+            if (isinstance(arg, correct_type)):
+                return old_function(arg)
+            else:
+                print("Bad Type")
+        return new_function
+    return check
+
+@type_check(int)
+def times2(num):
+    return num * 2
+
+print(times2(2))
+times2('Not A Number')
+
+@type_check(str)
+def first_letter(word):
+    return word[0]
+
+print(first_letter('Hello World'))
+first_letter(['Not', 'A', 'String'])
+
+print
+print("24 That's all, no more tutorials here. All Done.")
+print
